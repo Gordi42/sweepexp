c20e2e4a74b7df13000b916064bc581c2b25300d
Gordi42 <silvano98@hotmail.de>
2025-08-09 10:39:51 +0200

    move the logger setup into a function

--- src/sweepexp/logger.py
+++ src/sweepexp/logger.py
@@ -3,17 +3,23 @@ from __future__ import annotations
 
 import logging
 
-# Create a custom logger
-log: logging.Logger = logging.getLogger("sweepexp")
 
-# Create handlers
-c_handler: logging.StreamHandler = logging.StreamHandler()
+def _setup_logger() -> None:
+    # Create a custom logger
+    log: logging.Logger = logging.getLogger("sweepexp")
 
-# Create formatters and add it to handlers
-c_format: logging.Formatter = logging.Formatter(
-    "%(levelname)s - %(message)s",
-)
-c_handler.setFormatter(c_format)
+    # Create handlers
+    c_handler: logging.StreamHandler = logging.StreamHandler()
 
-# Add handlers to the logger
-log.addHandler(c_handler)
+    # Create formatters and add it to handlers
+    c_format: logging.Formatter = logging.Formatter(
+        "%(levelname)s - %(message)s",
+    )
+    c_handler.setFormatter(c_format)
+
+    # Add handlers to the logger
+    log.addHandler(c_handler)
+
+    return log
+
+log = _setup_logger()
